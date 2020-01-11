74c14af9152089a9a69bbdd2c7545f26340b5702
Christopher Robin Liljenström <Mallaceus@gmail.com>
2020-01-11 18:50:49 +0100

    TIMER

---
+++ Urist the Elfslayer/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerCountdown : MonoBehaviour
+{
+
+    public float timeLeft = 60.0f;
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0.0f)
+        {
+            TimerStop();
+        }
+    }
+
+    void TimerStop()
+    {
+        SceneManager.LoadSceneAsync("l3");
+    }
+}
